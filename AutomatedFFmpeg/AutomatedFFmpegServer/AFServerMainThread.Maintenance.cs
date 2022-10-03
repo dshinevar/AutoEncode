c45b4c7dd9a3ec075ca9e692006fb86ea128635b
Daniel Shinevar <danshinevar@gmail.com>
2022-10-03 11:14:30 -0500

    Moved clearing of jobs to the maintenance thread from the encoding job queue; Made logic more robust

--- AutomatedFFmpeg/AutomatedFFmpegServer/AFServerMainThread.Maintenance.cs
+++ AutomatedFFmpeg/AutomatedFFmpegServer/AFServerMainThread.Maintenance.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AutomatedFFmpegUtilities.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -19,38 +20,104 @@ namespace AutomatedFFmpegServer
         /// <summary>Clears out completed encoding jobs.</summary>
         private void ClearCompletedJobs()
         {
-            try
+            List<string> jobsRemovedLog = new();
+
+            // Encoded jobs that don't need post-processing
+            IReadOnlyList<EncodingJob> encodedJobs = EncodingJobQueue.GetEncodedEncodingJobs();
+            foreach (EncodingJob job in encodedJobs)
             {
-                var jobsRemoved = EncodingJobQueue.ClearCompletedJobs(Config.GlobalJobSettings.HoursCompletedUntilRemoval);
-                if (jobsRemoved?.Any() ?? false)
+                try
                 {
-                    List<string> jobsRemovedLog = new() { "Completed Jobs Removed" };
-                    jobsRemovedLog.AddRange(jobsRemoved);
-                    Logger.LogInfo(jobsRemovedLog, ThreadName);
+                    // If it's been completed for longer than the given number of hours, remove job
+                    TimeSpan ts = DateTime.Now.Subtract((DateTime)job.CompletedEncodingDateTime);
+                    if (ts.TotalHours >= Config.GlobalJobSettings.HoursCompletedUntilRemoval)
+                    {
+                        bool success = EncodingJobQueue.RemoveEncodingJob(job);
+                        if (success is true)
+                        {
+                            jobsRemovedLog.Add(job.ToString());
+                        }
+                        else
+                        {
+                            Logger.LogError($"Error removing completed {job} from queue.", ThreadName);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Logger.LogException(ex, $"Error removing completed {job} from queue.", ThreadName);
                 }
             }
-            catch (Exception ex)
+
+            // Jobs that were post-processed
+            IReadOnlyList<EncodingJob> postProcessedJobs = EncodingJobQueue.GetPostProcessedEncodingJobs();
+            foreach (EncodingJob job in postProcessedJobs)
             {
-                Logger.LogException(ex, "Error removing completed jobs.", ThreadName);
+                try
+                {
+                    // If it's been completed for longer than the given number of hours, remove job
+                    TimeSpan ts = DateTime.Now.Subtract((DateTime)job.CompletedPostProcessingTime);
+                    if (ts.TotalHours >= Config.GlobalJobSettings.HoursCompletedUntilRemoval)
+                    {
+                        bool success = EncodingJobQueue.RemoveEncodingJob(job);
+                        if (success is true)
+                        {
+                            jobsRemovedLog.Add(job.ToString());
+                        }
+                        else
+                        {
+                            Logger.LogError($"Error removing post-processed {job} from queue.", ThreadName);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Logger.LogException(ex, $"Error removing post-processed {job} from queue.", ThreadName);
+                }
+            }
+
+            if (jobsRemovedLog.Any())
+            {
+                jobsRemovedLog.Insert(0, "Completed Jobs Removed");
+                Logger.LogInfo(jobsRemovedLog, ThreadName);
             }
         }
 
         /// <summary>Clears out errored encoding jobs.</summary>
         private void ClearErroredJobs()
         {
-            try
+            List<string> jobsRemovedLog = new();
+
+            IReadOnlyList<EncodingJob> erroredJobs = EncodingJobQueue.GetErroredJobs();
+            foreach (EncodingJob job in erroredJobs)
             {
-                var jobsRemoved = EncodingJobQueue.ClearErroredJobs(Config.GlobalJobSettings.HoursErroredUntilRemoval);
-                if (jobsRemoved?.Any() ?? false)
+                try
                 {
-                    List<string> jobsRemovedLog = new() { "Errored Jobs Removed" };
-                    jobsRemovedLog.AddRange(jobsRemoved);
-                    Logger.LogInfo(jobsRemovedLog, ThreadName);
+                    // If it's been errored for longer than the given number of hours, remove job
+                    TimeSpan ts = DateTime.Now.Subtract((DateTime)job.ErrorTime);
+                    if (ts.TotalHours >= Config.GlobalJobSettings.HoursErroredUntilRemoval)
+                    {
+                        bool success = EncodingJobQueue.RemoveEncodingJob(job);
+                        if (success is true)
+                        {
+                            jobsRemovedLog.Add(job.ToString());
+                        }
+                        else
+                        {
+                            Logger.LogError($"Error removing errored {job} from queue.", ThreadName);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Logger.LogException(ex, $"Error removing errored {job} from queue.", ThreadName);
                 }
             }
-            catch (Exception ex)
+
+            if (jobsRemovedLog.Any())
             {
-                Logger.LogException(ex, "Error removing errored jobs.", ThreadName);
+                jobsRemovedLog.Insert(0, "Errored Jobs Removed");
+                Logger.LogInfo(jobsRemovedLog, ThreadName);
             }
         }
     }
